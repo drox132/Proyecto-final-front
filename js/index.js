6269713775a37ad3766fdfcf8cdf556267d17e21
giustin94 <natanaelgiusti@gmail.com>
2022-05-15 22:11:49 -0300

    lista de productos

--- js/index.js
+++ js/index.js
@@ -3,3 +3,39 @@ let name= "Mariano";
 function alerta(){
     alert("Compra realizada con exito, Disfruta tu producto");
 }
+
+const productos = [
+    {
+        id: 1,
+        nombre: "Auricular HyperX Cloud II",
+        precio: 15000,
+        img: "",
+        detalle: "Auriculares",
+        stock: 5
+    },
+    {
+        id: 2,
+        nombre: "Teclado SteelSeries Apex 3",
+        precio: 25000,
+        img: "",
+        detalle: "Teclado",
+        stock: 5
+    },
+    {
+        id: 3,
+        nombre: "Celular Moto G Power 2022",
+        precio: 50000,
+        img: "",
+        detalle: "Telefono celular",
+        stock: 5
+    },
+
+    {
+        id: 4,
+        nombre: "Monitor Sceptre",
+        precio: 35000,
+        img: "",
+        detalle: "Monitor HD",
+        stock: 5
+    },
+]
